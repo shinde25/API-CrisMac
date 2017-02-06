c629d0385d3c5570641c9f75955996f546817bae
shinde25 <25shinde25@gmail.com>
2017-02-06 17:22:06 +0530

    New updated changes

--- CrisMAcAPI/Areas/FAM/Models/AssetCommonModel.cs
+++ CrisMAcAPI/Areas/FAM/Models/AssetCommonModel.cs
@@ -14,6 +14,7 @@ namespace CrisMAcAPI.Areas.FAM.Models
         DatabaseProviderFactory factory = new DatabaseProviderFactory();
         public int AssetBlockAlt_key { get; set; }
         public int AssetBlockAlt { get; set; }
+        public int AssetBlockAlt3 { get; set; }
         public string AssetBlockName { get; set; }
         public string AssetBlockShortName { get; set; }
         public string AssetBlockValidCode { get; set; }
